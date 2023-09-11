93d162daba010bf2ce43bf64219876af8216a0ca
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2023-09-11 17:22:34 +0530

    initial firebase process

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+// import {initializeApp} from '@react-native-firebase/app';
 
+// Your Firebase project configuration
+// const firebaseConfig = {
+//   apiKey: 'YOUR_API_KEY',
+//   authDomain: 'YOUR_AUTH_DOMAIN',
+//   projectId: 'YOUR_PROJECT_ID',
+//   storageBucket: 'YOUR_STORAGE_BUCKET',
+//   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+//   appId: 'YOUR_APP_ID',
+// };
+
+// Initialize Firebase
+// initializeApp(firebaseConfig);
+
+// Register your app component
 AppRegistry.registerComponent(appName, () => App);
